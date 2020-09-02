ce4dd5687ec55bc0ebfb8a4449cd0e87e9b9a0b4
SosoPlayz <43059015+SosoPlayz@users.noreply.github.com>
2020-09-02 09:31:55 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -5,16 +5,11 @@ import time
 points = 0
 ua = UserAgent()
 
-print("\nEnter url:")
-url = input()
+url = input("\nEnter url\n>")
+ref = input("\nEnter referer (Press enter for none\n>")
+numb = int(input("\nEnter number of request(s)\n>"))
 
-print("\nEnter referer (Press enter for none)")
-ref = input()
-
-print("\nEnter number of requests")
-numb = int(input())
-
-print(f"\nProcessing {numb} requests for {url}")
+print(f"\nProcessing {numb} request(s) for {url}")
 start_time = time.time()
 for i in range(numb):
     start_time = time.time()
@@ -29,6 +24,6 @@ for i in range(numb):
     points += 1
     print(f"\nRequest #{points} with useragent {agent} Sent [{(time.time() - start_time)} Seconds]")
 
-print(f"\n\nSuccessfully spammed {url} {numb} times!")
+print(f"\nSuccessfully spammed {url} {numb} time(s)!\n")
 
-print("-------- Execution time --------\n--- %s seconds ---" % (time.time() - start_time))
+print("\n-------- Execution time --------\n--- %s seconds ---" % (time.time() - start_time))
